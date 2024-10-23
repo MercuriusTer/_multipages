14d1867af8885e98ff82b562c4bf534c235a6372
MercuriusTer <hakureimaiden115@gmail.com>
2024-10-23 17:57:52 +0700

    update img dir5

--- src/pages/Animation/Animation.jsx
+++ src/pages/Animation/Animation.jsx
@@ -133,7 +133,7 @@ function Animation() {
                             top: y,
                             position: 'relative',
                             transform: `rotate(${angle}deg)`,
-                            backgroundImage: ballType !== 'None' ? `url('/_multipages/src/assets/picture/ball/${ballType.toLowerCase()}.png')` : 'none',
+                            backgroundImage: ballType !== 'None' ? `url('/assets/ball/${ballType.toLowerCase()}.png')` : 'none',
                             backgroundSize: '115%',
                             backgroundPosition: 'center',
                         }}
